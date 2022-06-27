8fe992133ec16f638ef9c31726280900bdd724bb
raviteja <raviteja.venkatesh@costbo.com>
2022-06-27 20:16:16 +0530

    copy text to clipboard example in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function App() {
+  const [text, setText] = useState("Copy this text to clipboard");
+  const [isCopied, setIsCopied] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <div className="card">
+        <div className="card-header">
+          <h4 className="title">Copy Text to Clipboard</h4>
+        </div>
+
+        <div className="card-body">
+          <p className="label">Message</p>
+          <textarea
+            type="text"
+            value={text}
+            onChange={({ target }) => {
+              setText(target.value);
+            }}
+            placeholder="Enter Message"
+            className="text-area"
+          />
+
+          {isCopied ? (
+            <p className="success-msg">Text copied to clipboard</p>
+          ) : null}
+
+          <CopyToClipboard
+            text={text}
+            onCopy={() => {
+              setIsCopied(true);
+              setTimeout(() => {
+                setIsCopied(false);
+              }, 1000);
+            }}
+          >
+            <button className="btn">COPY</button>
+          </CopyToClipboard>
+
+          {/* Copy text to clipbaord using navigator */}
+          {/* <button
+            onClick={() => {
+              navigator.clipboard.writeText(text);
+              setIsCopied(true);
+              setTimeout(() => {
+                setIsCopied(false);
+              }, 1000);
+            }}
+            className="btn"
+          >
+            COPY
+          </button> */}
+        </div>
+      </div>
+
+      <textarea
+        type="text"
+        className="text-area"
+        style={{ marginTop: "10px" }}
+      />
     </div>
   );
 }
